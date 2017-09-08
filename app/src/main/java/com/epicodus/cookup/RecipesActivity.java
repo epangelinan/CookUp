449bb479c425f1476262135d92beeb86b462ee76
Guest User <Guest@epicodus-50.local>
2017-09-08 13:18:50 -0700

    add code to add data between activities

--- app/src/main/java/com/epicodus/cookup/RecipesActivity.java
+++ app/src/main/java/com/epicodus/cookup/RecipesActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.cookup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecipesActivity extends AppCompatActivity {
+    private TextView mIngredientTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+        mIngredientTextView = (TextView) findViewById(R.id.ingredientTextView);
+        Intent intent = getIntent();
+        String ingredient = intent.getStringExtra("ingredient");
+        mIngredientTextView.setText("Here are all the recipes containing " + ingredient);
     }
 }
