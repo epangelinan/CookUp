e00462755ceb9ef7206f71e3d74b7f7fb77a0020
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-08 10:10:59 -0700

    add RecipesActivity and intent to button

--- app/src/main/java/com/epicodus/cookup/MainActivity.java
+++ app/src/main/java/com/epicodus/cookup/MainActivity.java
@@ -1,13 +1,28 @@
 package com.epicodus.cookup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRecipesButton = (Button) findViewById(R.id.findRecipesButton);
+
+        mFindRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecipesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
