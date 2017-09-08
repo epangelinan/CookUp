519fff2360c75399b24e730721579b92e9ab92bf
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-08 10:42:16 -0700

    add EditText View

--- app/src/main/java/com/epicodus/cookup/MainActivity.java
+++ app/src/main/java/com/epicodus/cookup/MainActivity.java
@@ -3,23 +3,30 @@ package com.epicodus.cookup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRecipesButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.ingredientEditText);
         mFindRecipesButton = (Button) findViewById(R.id.findRecipesButton);
 
         mFindRecipesButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RecipesActivity.class);
                 startActivity(intent);
             }
