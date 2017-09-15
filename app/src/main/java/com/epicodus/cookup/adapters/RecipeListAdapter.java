5cb0bf94af06d13100141c98f52f019e3aadf860
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-15 11:51:05 -0700

    add ViewHolder ClickListener

--- app/src/main/java/com/epicodus/cookup/adapters/RecipeListAdapter.java
+++ app/src/main/java/com/epicodus/cookup/adapters/RecipeListAdapter.java
@@ -2,6 +2,7 @@ package com.epicodus.cookup.adapters;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 
 import com.epicodus.cookup.R;
 import com.epicodus.cookup.models.Recipe;
+import com.epicodus.cookup.ui.RecipeDetailActivity;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -44,7 +48,7 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
         return mRecipes.size();
     }
 
-    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @Bind(R.id.recipeImageView) ImageView mRecipeImageView;
         @Bind(R.id.recipeNameTextView) TextView mRecipeNameTextView;
         @Bind(R.id.sourceDisplayNameTextView) TextView mSourceDisplayNameTextView;
@@ -57,6 +61,16 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, RecipeDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("recipes", Parcels.wrap(mRecipes));
+            mContext.startActivity(intent);
         }
 
         public void bindRecipe(Recipe recipe) {
@@ -66,5 +80,4 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
             mRatingTextView.setText("Rating: " + recipe.getRating() + "/5");
         }
     }
-
 }
