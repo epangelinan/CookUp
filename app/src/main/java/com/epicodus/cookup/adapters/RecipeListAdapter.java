af04003881539b2dafaea6e282831b294e6c3e49
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-15 00:47:17 -0700

    add custom adapters with recyclerviews

---
+++ app/src/main/java/com/epicodus/cookup/adapters/RecipeListAdapter.java
@@ -0,0 +1,68 @@
+package com.epicodus.cookup.adapters;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epicodus.cookup.R;
+import com.epicodus.cookup.models.Recipe;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
+    private ArrayList<Recipe> mRecipes = new ArrayList<>();
+    private Context mContext;
+
+    public RecipeListAdapter(Context context, ArrayList<Recipe> recipes) {
+        mContext = context;
+        mRecipes = recipes;
+    }
+
+    @Override
+    public RecipeListAdapter.RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeListAdapter.RecipeViewHolder holder, int position) {
+        holder.bindRecipe(mRecipes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.recipeImageView) ImageView mRecipeImageView;
+        @Bind(R.id.recipeNameTextView) TextView mRecipeNameTextView;
+        @Bind(R.id.sourceDisplayNameTextView) TextView mSourceDisplayNameTextView;
+        @Bind(R.id.ratingTextView)
+        TextView mRatingTextView;
+
+        private Context mContext;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRecipe(Recipe recipe) {
+            mRecipeNameTextView.setText(recipe.getRecipeName());
+            mSourceDisplayNameTextView.setText(recipe.getSourceDisplayName());
+            mRatingTextView.setText("Rating: " + recipe.getRating() + "/5");
+        }
+    }
+
+}
--- app/src/main/java/com/epicodus/cookup/ui/RecipesActivity.java
+++ app/src/main/java/com/epicodus/cookup/ui/RecipesActivity.java
@@ -3,12 +3,15 @@ package com.epicodus.cookup.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.epicodus.cookup.R;
+import com.epicodus.cookup.adapters.RecipeListAdapter;
 import com.epicodus.cookup.models.Recipe;
 import com.epicodus.cookup.services.YummlyService;
 
@@ -23,18 +26,10 @@ import okhttp3.Response;
 
 public class RecipesActivity extends AppCompatActivity {
     public static final String TAG = RecipesActivity.class.getSimpleName();
-    @Bind(R.id.ingredientTextView) TextView mIngredientTextView;
-    @Bind(R.id.listView) ListView mListView;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RecipeListAdapter mAdapter;
 
     public ArrayList<Recipe> mRecipes = new ArrayList<>();
-
-//    private String[] recipes = new String[]{"Chicken Parmesan", "Buffalo Chicken Wings",
-//            "Chicken Masala", "Curry Chicken Salad", "Chicken Pot Pie", "Spicy Chicken Soup",
-//            "Honey-Garlic Chicken", "Rosemary Chicken Kabobs", "Chicken Tortilla Soup", "Chicken Cordon Bleu",
-//            "Indian Butter Chicken", "Holiday Chicken Salad", "Sweet Hot Mustard Chicken",
-//            "General Tsao's Chicken", "Chicken Asparagus Roll-ups"};
-//
-//    private String[] chefs = new String[]{"Kristine's Kitchen", "Southern Kissed", "The Weary Chef", "The Foodie Physician", "Byte Sized Nutrition", "Jessie and Melissa", "Family Food on the Table", "Mommy of a Monster", "The Recipe Critic", "The Cooking Jar", "Magic Skillet", "Bless This Mess", "Add a Pinch", "Betty Crocker", "Well Plated"};
 
 
     @Override
@@ -43,23 +38,10 @@ public class RecipesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
 
-//        CookUpArrayAdapter adapter = new CookUpArrayAdapter(this, android.R.layout.simple_list_item_1, recipes, chefs);
-//        mListView.setAdapter(adapter);
-
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                String recipe = ((TextView) view).getText().toString();
-//                Toast.makeText(RecipesActivity.this, recipe, Toast.LENGTH_LONG).show();
-//            }
-//        });
-
         Intent intent = getIntent();
         String ingredient = intent.getStringExtra("ingredient");
-      //  mIngredientTextView.setText("Recipes with " + ingredient);
 
         getRecipes(ingredient);
-
     }
 
     private void getRecipes(String ingredient) {
@@ -79,26 +61,13 @@ public class RecipesActivity extends AppCompatActivity {
 
                     @Override
                     public void run() {
-                        String[] recipeNames = new String[mRecipes.size()];
-                        for (int i = 0; i < recipeNames.length; i++) {
-                            recipeNames[i] = mRecipes.get(i).getRecipeName();
-                        }
+                        mAdapter = new RecipeListAdapter(getApplicationContext(), mRecipes);
 
-                        ArrayAdapter adapter = new ArrayAdapter(RecipesActivity.this,
-                                android.R.layout.simple_list_item_1, recipeNames);
-                        mListView.setAdapter(adapter);
-
-                        for (Recipe recipe : mRecipes) {
-                            Log.d(TAG, "imageUrlsBySize: " + recipe.getImageUrlsBySize());
-                            Log.d(TAG, "sourceDisplayName : " + recipe.getSourceDisplayName());
-                            Log.d(TAG, "ingredients : " + recipe.getIngredients().toString());
-                            Log.d(TAG, "id: " + recipe.getId());
-                            Log.d(TAG, "recipeName: " + recipe.getRecipeName());
-                            Log.d(TAG, "mTotalTimeInSeconds: " + recipe.getTotalTimeInSeconds());
-                            Log.d(TAG, "mRating: " + Double.toString(recipe.getRating()));
-
-                        }
-
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RecipesActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
                     }
                 });
             }
