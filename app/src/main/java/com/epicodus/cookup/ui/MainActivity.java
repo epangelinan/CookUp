fdaf8f563a4a21d94eb14e9dccfe69a1212ba888
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-21 15:54:13 -0700

    create and remove event listeners

--- app/src/main/java/com/epicodus/cookup/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/cookup/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Typeface;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +15,11 @@ import android.widget.Toast;
 
 import com.epicodus.cookup.Constants;
 import com.epicodus.cookup.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    private SharedPreferences.Editor mEditor;
 
     private DatabaseReference mSearchedIngredientReference;
+    private ValueEventListener mSearchedIngredientReferenceListener;
 
     @Bind(R.id.findRecipesButton) Button mFindRecipesButton;
     @Bind(R.id.aboutButton) Button mAboutButton;
@@ -38,8 +43,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchedIngredientReference = FirebaseDatabase
                 .getInstance()
                 .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_INGREDIENT);
+                .child(Constants.FIREBASE_CHILD_SEARCHED_INGREDIENT);//pinpoint ingredient node
 
+        mSearchedIngredientReferenceListener = mSearchedIngredientReference.addValueEventListener(new ValueEventListener() { //attach listener
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
+                for (DataSnapshot ingredientSnapshot : dataSnapshot.getChildren()) {
+                    String ingredient = ingredientSnapshot.getValue().toString();
+                    Log.d("Ingredients updated", "ingredient: " + ingredient); //log
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { //update UI here if error occurred.
+
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -86,5 +106,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
  //       private void addToSharedPreferences(String ingredient) {
  //           mEditor.putString(Constants.PREFERENCES_INGREDIENT_KEY, ingredient).apply();
  //       }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSearchedIngredientReference.removeEventListener(mSearchedIngredientReferenceListener);
+    }
 
 }
