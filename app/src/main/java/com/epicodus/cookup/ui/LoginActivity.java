ca6a3096d7be0ca770ba3fe0339dedbec30ec711
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-22 14:07:47 -0700

    remove some log statements

--- app/src/main/java/com/epicodus/cookup/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/cookup/ui/LoginActivity.java
@@ -99,9 +99,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         mAuthProgressDialog.dismiss();
-                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                       // Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
                         if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail", task.getException());
+                         //   Log.w(TAG, "signInWithEmail", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
