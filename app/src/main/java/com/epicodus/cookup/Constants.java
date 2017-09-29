a4aa83b648a9f61e136a05f2e29e3064c3d7d7e6
Esvethlynna Pangelinan <esveth@aol.com>
2017-09-29 09:06:23 -0700

    add extra spacing between lines to separate categories

--- app/src/main/java/com/epicodus/cookup/Constants.java
+++ app/src/main/java/com/epicodus/cookup/Constants.java
@@ -5,10 +5,13 @@ public class Constants {
     public static final String YUMMLY_APP_KEY = BuildConfig.YUMMLY_APP_KEY;
     public static final String YUMMLY_BASE_URL = "http://api.yummly.com/v1/api/recipes?";
     public static final String YUMMLY_INGREDIENT_QUERY_PARAMETER = "q";
+
     public static final String PREFERENCES_INGREDIENT_KEY = "ingredient";
+
     public static final String FIREBASE_CHILD_SEARCHED_INGREDIENT = "searchedIngredient";
     public static final String FIREBASE_CHILD_RECIPES = "recipes";
     public static final String FIREBASE_QUERY_INDEX = "index";
+
     public static final String EXTRA_KEY_POSITION = "position";
     public static final String EXTRA_KEY_RECIPES = "recipes";
     public static final String KEY_SOURCE = "source";
